2d31470628683e46a7b806fc5a237bf89e7a7733
kanwarsaab0007 <60256919+kanwarsaab0007@users.noreply.github.com>
2020-05-09 10:17:26 +0530

    aryan - updates
    to be varify by admin

--- algorithm.php
+++ algorithm.php
@@ -8,6 +8,7 @@
     <div class="container-fluid">
         make algorithms app here code everything inside this page we'll create team page leter
         after making changes push to github using github desktop app
+        these changes are made on Aryan's PC it should be updated on everyone's project after beign approved from admin on github........
     </div>
   </body>
 </html>
